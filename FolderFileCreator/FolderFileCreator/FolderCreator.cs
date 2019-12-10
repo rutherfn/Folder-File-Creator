76075f9d4ad3aa09219b253802bc053f7badc314
rutherfn12 <nicholasrutherford121@gmail.com>
2019-12-10 16:42:58 -0600

    full program completed, may have to go back and refactor some code around

--- FolderFileCreator/FolderFileCreator/FolderCreator.cs
+++ FolderFileCreator/FolderFileCreator/FolderCreator.cs
@@ -8,34 +8,59 @@ namespace FolderFileCreator
 {
     class FolderCreator
     {
+        bool createNewDirectory;
         string pathString = "";
-        String subFolder = "";
+        bool userChoice;
+        string userTryAgainChoice = "";
+        string subFolder = "";
         bool directoryExist = false;
         string topLevelFolderName = "";
         string userCreateFolderChoice = "";
         string tryAgain = "";
         public void createFolder()
         {
-            Console.Clear();
-            Console.WriteLine("Currently your folder location is " + "\t\n" +  System.AppDomain.CurrentDomain.BaseDirectory + " \t\nWould you like to change that?\t\nPlease enter yes or no");
-            userCreateFolderChoice = Console.ReadLine();
-            if(userCreateFolderChoice == "yes" || userCreateFolderChoice == "Yes")
+            do
             {
-                changeFolderCurrentLocation();
-                if (tryAgain == "1")
+                Console.Clear();
+                Console.WriteLine("\t\nCurrently your folder location is " + "\t\n" + System.AppDomain.CurrentDomain.BaseDirectory + " \t\n\t\nWould you like to change that?\t\n\t\nPlease enter Yes or No");
+                userCreateFolderChoice = Console.ReadLine();
+                if (userCreateFolderChoice == "yes" || userCreateFolderChoice == "Yes")
                 {
+                    changeFolderCurrentLocation();
+                    if (tryAgain == "1")
+                    {
+                        createFolderUnderTopLevel();
+                        createDirectory();
+                        askUserIfTheyWantToCreateANewFile();
+                        userChoice = true;
+                    }
+                }
+                else if (userCreateFolderChoice == "no" || userCreateFolderChoice == "No")
+                {
+                    topLevelFolderName = System.AppDomain.CurrentDomain.BaseDirectory;
                     createFolderUnderTopLevel();
                     createDirectory();
                     askUserIfTheyWantToCreateANewFile();
+                    userChoice = true;
                 }
-            }
-            else
-            {
-                topLevelFolderName = System.AppDomain.CurrentDomain.BaseDirectory;
-                createFolderUnderTopLevel();
-                createDirectory();
-                askUserIfTheyWantToCreateANewFile();
-            }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Wrong Entry would you like to try again?\t\n\t\nEnter 1 for Yes Or Any Other Chracter For No");
+
+                    userTryAgainChoice = Console.ReadLine();
+                    if(userTryAgainChoice == "1")
+                    {
+                        Console.Clear();
+                        userChoice = false;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        userChoice = true;
+                    }
+                }
+            } while (userChoice == false);
         }
         private void changeFolderCurrentLocation()
         {
@@ -52,9 +77,29 @@ namespace FolderFileCreator
                 {
                     Console.WriteLine("\t\nDirectory does not exist will you like to try again?\t\n1:Yes\t\n2:No");
                     tryAgain = Console.ReadLine();
-                    if(tryAgain == "2")
+                    if(tryAgain == "1")
+                    {
+                        directoryExist = false;
+                    }
+                    else if(tryAgain == "2")
                     {
                         directoryExist = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong Entry would you like to try again?\t\n\t\nEnter 1 for Yes Or Any Other Chracter For No");
+
+                        userTryAgainChoice = Console.ReadLine();
+                        if (userTryAgainChoice == "1")
+                        {
+                            Console.Clear();
+                            directoryExist = false;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            directoryExist = true;
+                        }
                     }
                 }
             } while (directoryExist == false);
@@ -75,15 +120,39 @@ namespace FolderFileCreator
         }
         private void askUserIfTheyWantToCreateANewFile()
         {
-            string userChoice = "";
-            Console.Clear();
-            Console.WriteLine("\t\nCongrats you have now just created a new sub folder called: " + subFolder + "\t\nWould you like to create a new file under this folder?\t\n1: For Yes\t\n2: For No");
-            userChoice = Console.ReadLine();
-            if(userChoice == "1")
+            do
             {
-                FileCreator fileCreator = new FileCreator();
-                fileCreator.createNewFileWhenNewFolderCreated(pathString);
-            }
+                string userChoice = "";
+                Console.Clear();
+                Console.WriteLine("\t\nCongrats you have now just created a new sub folder called: " + subFolder + "\t\nWould you like to create a new file under this folder?\t\n1: For Yes\t\n2: For No");
+                userChoice = Console.ReadLine();
+                if (userChoice == "1")
+                {
+                    createNewDirectory = true;
+                    FileCreator fileCreator = new FileCreator();
+                    fileCreator.createNewFileWhenNewFolderCreated(pathString);
+                }
+                else if (userChoice == "2")
+                {
+                    createNewDirectory = true;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Entry would you like to try again?\t\n\t\nEnter 1 for Yes Or Any Other Chracter For No");
+
+                    userTryAgainChoice = Console.ReadLine();
+                    if (userTryAgainChoice == "1")
+                    {
+                        Console.Clear();
+                        createNewDirectory = false;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        createNewDirectory = true;
+                    }
+                }
+            } while (createNewDirectory == false);
         }
     }
 }
